2888cdf9c83466208b22b736d0c5df128cffd0b1
Mohammed <moohyacc@gmail.com>
2020-02-02 11:25:07 +0300

    indexes problems fixing by adding id and unq

--- db/migrate/20200127103525_create_galleries.rb
+++ db/migrate/20200127103525_create_galleries.rb
@@ -2,9 +2,10 @@ class CreateGalleries < ActiveRecord::Migration[6.0]
   def change
     create_table :galleries do |t|
       t.text :url
-      t.references :service
+      t.references :service, null: false, foreign_key: true
 
       t.timestamps
     end
+    add_index :galleries, :service_id, unique: true
   end
 end
