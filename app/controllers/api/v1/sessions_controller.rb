7c928b96ba8ced5ac73d53b3c83b3fb6816ad678
Mohammed <moohyacc@gmail.com>
2020-02-04 12:29:01 +0300

    user_type when login

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::SessionsController < Devise::SessionsController
     private
 
     def respond_with(resource, _opts = {})
-      render json: {user: resource, token: current_token}
+      render json: {user: resource, user_type: resource.role.title, token: current_token}
     end
   
     def respond_to_on_destroy
