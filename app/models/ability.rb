60ee3da911fe41eabc109aaa728aacfb40def254
Mohammed <moohyacc@gmail.com>
2020-02-04 21:10:14 +0300

    category cancan permissions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -34,6 +34,7 @@ class Ability
         can :read, Bank
       else
         can :read, Service
+        can :read, Category
       end
     #
     # The first argument to `can` is the action you are giving the user
