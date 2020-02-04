a56e00d2f9baf6d4d7508d3eb4af7391a3b37aac
Mohammed <moohyacc@gmail.com>
2020-02-04 12:41:26 +0300

    datetime spelling

--- db/migrate/20200204000542_add_expiration_time_to_jwt_blacklist.rb
+++ db/migrate/20200204000542_add_expiration_time_to_jwt_blacklist.rb
@@ -1,6 +1,6 @@
 class AddExpirationTimeToJwtBlacklist < ActiveRecord::Migration[6.0]
   def change
-    add_column :jwt_blacklist, :exp, :datatime
+    add_column :jwt_blacklist, :exp, :datetime
     change_column_null :jwt_blacklist, :exp, false
   end
 end
