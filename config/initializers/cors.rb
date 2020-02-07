bcbf389cdbfe8595f2cff9673f518f772dfee955
Mohammed <moohyacc@gmail.com>
2020-02-07 15:56:59 +0300

    trying cors http w s

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://mighty-hamlet-26810.herokuapp.com'
+    origins 'https://mighty-hamlet-26810.herokuapp.com'
 
     resource '*',
       headers: :any,
